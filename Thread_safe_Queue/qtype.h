31b8c71888df72ee66851d906e41969ed1123518
박정웅 <lgchu10@naver.com>
2025-05-26 14:12:57 +0900

    예제 코드 작성.

--- Thread_safe_Queue/qtype.h
+++ Thread_safe_Queue/qtype.h
@@ -1,1 +1,23 @@
 #pragma once
+
+typedef struct {
+	Key key;
+	Value value;
+} Item;
+
+typedef struct {
+	bool success;   // true: 성공, false: 실패
+	Item item;
+	// 필드추가가능
+	} Reply;
+
+typedef struct node_t {
+	Item item;
+	struct node_t* next;
+	// 필드추가가능
+} Node;
+
+typedef struct {
+	Node* head, tail;
+	// 필드추가가능
+	} Queue;
---
+++ Thread_safe_Queue/queue.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "queue.h"
+
+
+Queue* init(void) {
+	return NULL;
+}
+
+
+void release(Queue* queue) {
+	return;
+}
+
+
+Node* nalloc(Item item) {
+	//Node 생성, item으로 초기화
+	return NULL;
+}
+
+
+void nfree(Node* node) {
+	return;
+}
+
+
+Node* nclone(Node* node) {
+	return NULL;
+}
