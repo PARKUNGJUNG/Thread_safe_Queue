9df35cc28f169bb0ffcda952bcad98a073c04047
박정웅 <lgchu10@naver.com>
2025-05-29 13:32:21 +0900

    qtype.h 자료구조 설계 개선 및 노드 도입

--- Thread_safe_Queue/qtype.h
+++ Thread_safe_Queue/qtype.h
@@ -18,6 +18,8 @@ typedef struct node_t {
 } Node;
 
 typedef struct {
-	Node* head, tail;
-	// 필드추가가능
-	} Queue;
+	Node* head, * tail;
+	std::mutex lock; // 뮤텍스 추가
+	size_t count; //수 추적
+} Queue;
+//더미 헤드 노드 추가: head ->  next를 첫 노드로 사용해 경합 조건 감소
